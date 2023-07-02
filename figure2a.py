6ab5d91f6d7343062927fac06e8d1eacd6c56670
Justin Reese <justaddcoffee@gmail.com>
2023-07-02 10:25:52 -0400

    Add updated code

--- figure2a.py
+++ figure2a.py
@@ -9,8 +9,8 @@ data_dir = 'data'
 file1_name = 'Table 1. ChatGPT4 Diagnosis - text without discussion - txt_cases_results.tsv'
 file2_name = 'Table 2. ChatGPT4 Diagnosis - only age_sex_signs_symptoms - phenopacket_based_queries_results.tsv'
 
-file1_label = 'Narative-based case description'
-file2_label = 'Feature-based case description'
+file1_label = 'Narrative-based queries'
+file2_label = 'Feature-based queries'
 
 file1 = pd.read_csv(os.path.join(data_dir, file1_name), sep="\t", header=0)
 file2 = pd.read_csv(os.path.join(data_dir, file2_name), sep="\t", header=0)
@@ -34,22 +34,54 @@ def to_numeric(s: pd.Series) -> pd.Series:
 score_counts_file1 = to_numeric(df['file1_score']).value_counts().sort_index()
 score_counts_file2 = to_numeric(df['file2_score']).value_counts().sort_index()
 
-# Create a grouped histogram
-fig, ax = plt.subplots()
+# Create the first grouped histogram
+fig1, ax1 = plt.subplots()
 bar_width = 0.35
 index = score_counts_file1.index
 
-bar1 = ax.bar(index - bar_width/2, score_counts_file1, bar_width, label=file1_label)
-bar2 = ax.bar(index + bar_width/2, score_counts_file2, bar_width, label=file2_label)
+bar1 = ax1.bar(index - bar_width/2, score_counts_file1, bar_width, label=file1_label)
+bar2 = ax1.bar(index + bar_width/2, score_counts_file2, bar_width, label=file2_label)
 
-ax.set_xlabel('Score')
-ax.set_ylabel('Count')
-ax.set_title('A')
-ax.set_xticks(index)
-ax.set_xticklabels(index)
-ax.legend()
+ax1.set_xlabel('Score')
+ax1.set_ylabel('Count')
+ax1.set_title('A')
+ax1.set_xticks(index)
+ax1.set_xticklabels(index)
+ax1.legend()
 
-plt.show()
+# Save the first figure to a PNG file
+fig1.savefig('figure2a.png')
 
-# write out plot to file
-fig.savefig('figure2a.png')
+# Create the second grouped histogram (duplicate)
+fig2, ax2 = plt.subplots()
+bar3 = ax2.bar(index - bar_width/2, score_counts_file1, bar_width, alpha=0.5, label=file1_label)
+bar4 = ax2.bar(index + bar_width/2, score_counts_file2, bar_width, alpha=0.5, label=file2_label)
+
+ax2.set_xlabel('Score')
+ax2.set_ylabel('Count')
+ax2.set_title('B')
+ax2.set_xticks(index)
+ax2.set_xticklabels(index)
+ax2.legend()
+
+# Save the second figure to a PNG file
+fig2.savefig('figure2b.png')
+
+# Combine both figures side by side into a single PNG file
+combined_fig = plt.figure(figsize=(12, 6))
+combined_fig.add_subplot(1, 2, 1)
+combined_fig.add_subplot(1, 2, 2)
+
+img1 = plt.imread('figure2a.png')
+img2 = plt.imread('figure2b.png')
+
+plt.subplot(1, 2, 1)
+plt.imshow(img1)
+plt.axis('off')
+
+plt.subplot(1, 2, 2)
+plt.imshow(img2)
+plt.axis('off')
+
+# Save the combined figure to a PNG file
+combined_fig.savefig('combined_figure.png')
